487d69bf07a425c7a05bd00d82ecfbad3f642920
Raju Gautam <rajugautam45@gmail.com>
2018-10-21 14:49:20 +0530

    changes

--- index.js
+++ index.js
@@ -1,37 +1,8 @@
 const express =  require('express');
-const { spawn } = require('child_process');
-
 const app = express();
-
-let scraper = (req) => {
-    
-    return new Promise( (resolve, reject)=>{
-
-    
-    const childProcess =   spawn('python',['./flipkart_scraper.py',req.query.psearch]);
-    
-    childProcess.stdout.on('data', function(data) { 
-         resolve(data)
-    }); 
-    childProcess.stderr.on('data',(data)=>{
-           reject(error)
-           
-    })
-      
-});    
-
-}
-app.get('/getScrapedData',  (req,res)=>{
-    res.write("")
-    scraper(req).then((scrapedData)=>{
-        res.end(scrapedData)
-    })
-    .catch((err) => {
-        console.log("Error occured: " +err)
-    })
-});
+require('./pythonScraperModule/trigger')(app);
+require('./nodeScraperModule/amazonScraper')(app);
 
 app.listen(3000,()=>{
     console.log("Running on 3000");
 });
-
