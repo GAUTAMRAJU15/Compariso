7ccb7024d7719b43ae693b370577084c2f3178ef
Raju Gautam <rajugautam45@gmail.com>
2018-10-21 14:49:04 +0530

    refactored python scraper

---
+++ pythonScraperModule/flipkartScraper.py
@@ -0,0 +1,57 @@
+import urllib2
+from BeautifulSoup import BeautifulSoup
+import re
+import sys
+# i =0
+
+url = "https://www.flipkart.com/search?q="+ sys.argv[1]
+text = urllib2.urlopen(url).read()
+soup = BeautifulSoup(text)
+
+payload = {
+'product_name': [],
+'product_price': [],
+'product_url': []
+}
+
+product_name = soup.findAll('div',attrs={'class':'_3wU53n'})
+product_price = soup.findAll('div',attrs={'class': '_1vC4OE _2rQ-NK'})
+for a in soup.findAll('a',href=True):
+    if re.findall('pid', a['href']):
+        product_url =  a['href']
+        payload['product_url'].append(product_url)
+
+
+
+for div in product_name:
+    links = div.findAll(text=True)
+    for a in links:
+        payload['product_name'].append(a)
+
+for div in product_price:
+    links = div.findAll(text=True)
+    for a in links:
+        payload['product_price'].append(a)
+
+
+f= open("out.txt","w+")
+i = 1;
+
+while(i < len(payload['product_name'])):
+	ppname =  ''
+	pprice =  ''
+	plink= ''
+
+	for name in range(0,i):
+		ppname = payload['product_name'][name]
+	for price in range(0,i):
+		pprice =  payload['product_price'][name]
+	for link in range(0,i):
+		plink =  payload['product_url'][name]
+
+	sys.stdout.write(str(ppname) +" && "+ str(pprice) + " && " + "https://flipkart.com"+str(plink) + "\n\n")
+	i= i+1;
+
+
+f.close()
+sys.stdout.flush()
---
+++ pythonScraperModule/trigger.js
@@ -0,0 +1,32 @@
+
+const { spawn } = require('child_process');
+
+
+  let scraper = (req) => {
+
+      return new Promise( (resolve, reject)=>{
+
+      const childProcess =   spawn('python',["./pythonScraperModule/flipkartScraper.py",req.query.psearch]);
+
+      childProcess.stdout.on('data', function(data) {
+           resolve(data)
+      });
+      childProcess.stderr.on('data',(data)=>{
+             reject(data)
+      });
+
+   });
+
+  }
+  module.exports = (app) => {
+
+  app.get('/getScrapedData',  (req,res)=>{
+      res.write("")
+      scraper(req).then((scrapedData)=>{
+          res.end(scrapedData)
+      })
+      .catch((err) => {
+          console.log("Error occured: " +err)
+      })
+  });
+}
